7e589a8a10cd547fe22cfe647af118dfbafd48bf
Pratik Kale <ppvkale@gmail.com>
2023-12-17 10:57:33 +0530

    Left Drawer for Filters

--- app/all/page.tsx
+++ app/all/page.tsx
@@ -12,6 +12,8 @@ import {
   SelectChangeEvent,
   Rating,
 } from "@mui/material";
+import TemporaryDrawer from "@/components/Drawer";
+
 
 const AllProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -29,12 +31,11 @@ const AllProducts = () => {
     fetch(apiUrl)
       .then((res) => res.json())
       .then((json) => {
-    
         const filteredProducts = json.filter((product: Product) => {
           if (rating) {
             return product.rating.rate >= parseInt(rating, 10);
           }
-          return true; 
+          return true;
         });
 
         if (price === "highLow") {
@@ -46,7 +47,6 @@ const AllProducts = () => {
         setProducts(filteredProducts);
       });
   }, [category, rating, price]);
-
 
   const handleCategoryChange = (event: SelectChangeEvent) => {
     setCategory(event.target.value as string);
@@ -66,69 +66,101 @@ const AllProducts = () => {
     setPrice("");
   };
 
-
   return (
     <div>
       <Container>
-        <div className="sticky z-30 grid grid-cols-3 gap-3 px-2 py-4 mt-6 bg-white border rounded-md md:p-4 md:gap-20 top-16 border-slate-300">
-          <div>
-            <FormControl variant="standard" fullWidth>
-              <InputLabel className="text-xs md:text-base" id="product-category-label">Sort by Categories</InputLabel>
-              <Select
-                labelId="product-category-label"
-                id="product-category"
-                value={category}
-                label="Category"
-                onChange={handleCategoryChange}
-              >
-                <MenuItem value="men's clothing">Men&apos;s Clothing</MenuItem>
-                <MenuItem value="women's clothing">Women&apos;s Clothing</MenuItem>
-                <MenuItem value="electronics">Electronics</MenuItem>
-                <MenuItem value="jewelery">Jewlery</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
+        <TemporaryDrawer>
+          <div className="p-4">
+            <h1 className="text-xl font-bold text-violet-700">Filters</h1>
+            <div>
+              <FormControl variant="standard" fullWidth>
+                <InputLabel
+                  className="text-xs md:text-base"
+                  id="product-category-label"
+                >
+                  Sort by Categories
+                </InputLabel>
+                <Select
+                  labelId="product-category-label"
+                  id="product-category"
+                  value={category}
+                  label="Category"
+                  onChange={handleCategoryChange}
+                >
+                  <MenuItem value="men's clothing">
+                    Men&apos;s Clothing
+                  </MenuItem>
+                  <MenuItem value="women's clothing">
+                    Women&apos;s Clothing
+                  </MenuItem>
+                  <MenuItem value="electronics">Electronics</MenuItem>
+                  <MenuItem value="jewelery">Jewlery</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
 
-          <div>
-            <FormControl variant="standard" fullWidth>
-              <InputLabel className="text-xs md:text-base" id="product-reviews-label">Sort by Reviews</InputLabel>
-              <Select
-                labelId="product-reviews-label"
-                id="product-reviews"
-                value={rating}
-                label="rating"
-                onChange={handleReviewChange}
-              >
-                <MenuItem value={4}><Rating value={4} readOnly /></MenuItem>
-                <MenuItem value={3}><Rating value={3} readOnly /></MenuItem>
-                <MenuItem value={2}><Rating value={2} readOnly /></MenuItem>
-                <MenuItem value={1}><Rating value={1} readOnly /></MenuItem>
-              </Select>
-            </FormControl>
-          </div>
+            <div>
+              <FormControl variant="standard" fullWidth>
+                <InputLabel
+                  className="text-xs md:text-base"
+                  id="product-reviews-label"
+                >
+                  Sort by Reviews
+                </InputLabel>
+                <Select
+                  labelId="product-reviews-label"
+                  id="product-reviews"
+                  value={rating}
+                  label="rating"
+                  onChange={handleReviewChange}
+                >
+                  <MenuItem value={4}>
+                    <Rating value={4} readOnly />
+                  </MenuItem>
+                  <MenuItem value={3}>
+                    <Rating value={3} readOnly />
+                  </MenuItem>
+                  <MenuItem value={2}>
+                    <Rating value={2} readOnly />
+                  </MenuItem>
+                  <MenuItem value={1}>
+                    <Rating value={1} readOnly />
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </div>
 
-          <div>
-          <FormControl variant="standard" fullWidth>
-              <InputLabel className="text-xs md:text-base" id="product-price-label">Sort by Price</InputLabel>
-              <Select
-                labelId="product-price-label"
-                id="product-price"
-                value={price}
-                label="price"
-                onChange={handlePriceChange}
-              >
-                <MenuItem value={"highLow"}>High to Low</MenuItem>
-                <MenuItem value={"lowHigh"}>Low to High</MenuItem>
-              </Select>
-            </FormControl>
+            <div>
+              <FormControl variant="standard" fullWidth>
+                <InputLabel
+                  className="text-xs md:text-base"
+                  id="product-price-label"
+                >
+                  Sort by Price
+                </InputLabel>
+                <Select
+                  labelId="product-price-label"
+                  id="product-price"
+                  value={price}
+                  label="price"
+                  onChange={handlePriceChange}
+                >
+                  <MenuItem value={"highLow"}>High to Low</MenuItem>
+                  <MenuItem value={"lowHigh"}>Low to High</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
           </div>
+          <div className="text-center">
+          <button
+            onClick={handleClearFilters}
+            className="px-6 py-1 mt-2 text-base text-white rounded-md bg-violet-700"
+          >
+            Clear
+          </button>
         </div>
-        <div className="text-center">
-        <button onClick={handleClearFilters} className="px-4 py-1 mt-2 text-sm text-white rounded-sm bg-violet-700" >
-              Clear
-            </button>
-        </div>
-        <div className="grid grid-cols-2 gap-8 mt-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        </TemporaryDrawer>
+        <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
           {products.map((product: Product) => (
             <div key={product.id}>
               <ProductCard data={product} />
---
+++ components/Drawer.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { ImCross } from "react-icons/im";
+
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+interface TemporaryDrawerProps { 
+    children: React.ReactNode
+    }
+
+const TemporaryDrawer: React.FC<TemporaryDrawerProps> = ({ children }) => {
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+        <div className='flex items-end justify-end mt-2'><Button onClick={toggleDrawer(anchor, false)} ><ImCross className='text-red-500'/></Button></div>
+        {children}
+    </Box>
+  );
+
+  return (
+    <div>
+      {(['left'] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button className="mt-3 text-white normal-case bg-violet-700 hover:bg-violet-600" onClick={toggleDrawer(anchor, true)}>Filters</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}            
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+export default TemporaryDrawer;
